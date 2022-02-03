bc541de7c229891248c0c959c55a41b75a412135
vtitoff <v.titoff2010@gmail.com>
2022-02-04 01:44:19 +0300

    header experiments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import {useState} from 'react';
 import WeatherItemTemp from './components/WeatherItem/WeatherItemTemp';
 import WeatherItemMaxMin from './components/WeatherItem/WeatherItemMaxMin';
 import WeatherItemPressure from './components/WeatherItem/WeatherItemPressure';
@@ -8,97 +8,114 @@ import WeatherItemWind from './components/WeatherItem/WeatherItemWind';
 import Title from './components/Title/Title';
 
 
-
 function App() {
-  let data = {
-    "coord": {
-      "lon": 30.3364,
-      "lat": 53.9139
-    },
-    "weather": [
-      {
-        "id": 802,
-        "main": "Clouds",
-        "description": "переменная облачность",
-        "icon": "03d"
-      }
-    ],
-    "base": "stations",
-    "main": {
-      "temp": 21.06,
-      "feels_like": 20.27,
-      "temp_min": 21.06,
-      "temp_max": 21.06,
-      "pressure": 1019,
-      "humidity": 40
-    },
-    "visibility": 10000,
-    "wind": {
-      "speed": 4,
-      "deg": 265
-    },
-    "clouds": {
-      "all": 40
-    },
-    "dt": 1629632677,
-    "sys": {
-      "type": 1,
-      "id": 8938,
-      "country": "BY",
-      "sunrise": 1629600566,
-      "sunset": 1629652443
-    },
-    "timezone": 10800,
-    "id": 625665,
-    "name": "Mogilev",
-    "cod": 200
-  }
-  const [weatherData, setWeatherData] = useState(data);
-  let city = 'Mogilev'
-  // fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=ru&appid=19c0bee458ab65a261e79f492074b660`)
-  //   .then(response => response.json())
-  //   .then(json => setWeatherData(json)) // потом включить, пока заменим запрос с сервера статичным json
+    let data = {
+        "coord": {
+            "lon": 30.3364,
+            "lat": 53.9139
+        },
+        "weather": [
+            {
+                "id": 802,
+                "main": "Clouds",
+                "description": "переменная облачность",
+                "icon": "03d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 21.06,
+            "feels_like": 20.27,
+            "temp_min": 21.06,
+            "temp_max": 21.06,
+            "pressure": 1019,
+            "humidity": 40
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 4,
+            "deg": 265
+        },
+        "clouds": {
+            "all": 40
+        },
+        "dt": 1629632677,
+        "sys": {
+            "type": 1,
+            "id": 8938,
+            "country": "BY",
+            "sunrise": 1629600566,
+            "sunset": 1629652443
+        },
+        "timezone": 10800,
+        "id": 625665,
+        "name": "Mogilev",
+        "cod": 200
+    }
+    const [weatherData, setWeatherData] = useState(data);
+    let city = 'Mogilev'
+    // fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=ru&appid=19c0bee458ab65a261e79f492074b660`)
+    //   .then(response => response.json())
+    //   .then(json => setWeatherData(json)) // потом включить, пока заменим запрос с сервера статичным json
 
 
-  // useEffect(() => {
-  //   const L = require('leaflet');
-  //   let current_lat = 53.9139;
-  //   let current_long = 30.3364;
-  //   let current_zoom = 16;
-  //   let center_lat = current_lat;
-  //   let center_long = current_long;
-  //   let center_zoom = current_zoom;
-  //   let map = L.map('map', {
-  //     center: [center_lat, center_long],
-  //     zoom: center_zoom
-  //   });
-  //   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  //     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  //   }).addTo(map);
-  // });
+    // useEffect(() => {
+    //   const L = require('leaflet');
+    //   let current_lat = 53.9139;
+    //   let current_long = 30.3364;
+    //   let current_zoom = 16;
+    //   let center_lat = current_lat;
+    //   let center_long = current_long;
+    //   let center_zoom = current_zoom;
+    //   let map = L.map('map', {
+    //     center: [center_lat, center_long],
+    //     zoom: center_zoom
+    //   });
+    //   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    //     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //   }).addTo(map);
+    // });
 
 
-  return (
-    <div className="App">
-      <style>@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap')</style>
-      <div className="container">
-        <Title text={city}></Title>
-        <Title text="Сегодня"></Title>
-        <div className="weather__items-inner">
-          <div>
-            <WeatherItemMaxMin temp_min={weatherData.main.temp_min} temp_max={weatherData.main.temp_max}></WeatherItemMaxMin>
-            <WeatherItemPressure pressure={weatherData.main.pressure}></WeatherItemPressure>
-          </div>
-          <div>
-            <WeatherItemTemp temp={weatherData.main.temp} desc={weatherData.weather[0].description} image={weatherData.weather[0].icon}></WeatherItemTemp>
-            <WeatherItemWind wind_speed={weatherData.wind.speed} wind_deg={weatherData.wind.deg}></WeatherItemWind>
-          </div>
-          {/*<WeatherMap></WeatherMap>*/}
-          {/* <div className={classes.weatherMap} id="map"></div> */}
+    return (
+        <div className="App">
+            <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow navbar__header">
+                <form
+                    className="navbar__form">
+                    <div className="input navbar__input">
+                        <input type="text" className="form-control bg-light border-0 small" placeholder="Search for..."
+                               aria-label="Search" aria-describedby="basic-addon2"/>
+                        <div className="input-group-append">
+                            <button className="btn btn-primary" type="button">
+                                <i className="fas fa-search fa-sm"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </nav>
+            <style>@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap')
+            </style>
+            <div className="container">
+                <Title text={city}></Title>
+                <Title text="Сегодня"></Title>
+                <div className="weather__items-inner">
+                    <div>
+                        <WeatherItemMaxMin temp_min={weatherData.main.temp_min}
+                                           temp_max={weatherData.main.temp_max}></WeatherItemMaxMin>
+                        <WeatherItemPressure pressure={weatherData.main.pressure}></WeatherItemPressure>
+                    </div>
+                    <div>
+                        <WeatherItemTemp temp={weatherData.main.temp} desc={weatherData.weather[0].description}
+                                         image={weatherData.weather[0].icon}></WeatherItemTemp>
+                        <WeatherItemWind wind_speed={weatherData.wind.speed}
+                                         wind_deg={weatherData.wind.deg}></WeatherItemWind>
+                    </div>
+                    {/*<WeatherMap></WeatherMap>*/}
+                    {/* <div className={classes.weatherMap} id="map"></div> */}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
- import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-      integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-      crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-      integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-      crossorigin=""></script>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossOrigin="anonymous"/>
+
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+              integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+              crossorigin=""/>
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+                integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+                crossorigin=""></script>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
